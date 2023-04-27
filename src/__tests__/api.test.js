42031b64989e8ea7a0b7974664ed4c57457c90d1
Kevin La <chenchiaho@gmail.com>
2023-04-27 08:22:20 +0800

    mock api call encounter error

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -1,7 +1,26 @@
 describe('API', () => {
+    let originalFetch;
+
     beforeAll(() => {
-        global.fetch = jest.fn(() => {
-            Promise
+        originalFetch = global.fetch;
+        jest.mock(global, 'fetch').mockImplementation(() => {
+            console.log('Mocked fetch called');
+            return Promise.resolve({
+                json: () => Promise.resolve({
+                    data: 'Hello World'
+                })
+            })
         })
+    })
+
+    afterAll(() => {
+        global.fetch.mockRestore();
+    })
+
+    it('should return the correct data', async () => {
+        const response = await global.fetch('https://example.com/api');
+        const data = await response.json();
+        console.log('Data:', response);
+        expect(data.data).toBe('Hello World');
     })
 })
